268630e3f8ac66e0ad7e2274f1caae0747dc17ae
Felipe Melo <narulee@hotmail.com>
2022-05-06 18:35:34 -0300

    web frontend api integration, loading button

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -6,6 +6,9 @@ import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../ClosedButton";
 import { ScreenshotButton } from "../ScreenshotButton";
 
+import { api } from "../../../services/api";
+import { Loading } from "../../Loading";
+
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType,
   onFeedbackRestartRequested: () => void;
@@ -19,17 +22,22 @@ export function FeedbackContentStep({
 }: FeedbackContentStepProps) {
   const [screenshot, setSreenshot] = useState<string | null>(null)
   const [comment, setComment] = useState('')
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false)
 
   const feedbackTypeInfo = feedbackTypes[feedbackType]
 
-  function handleSubmitFeedback(event: FormEvent) {
+  async function handleSubmitFeedback(event: FormEvent) {
     event.preventDefault()
 
-    console.log({
-      screenshot,
-      comment
+    setIsSendingFeedback(true)
+
+    await api.post('/feedbacks', {
+      type: feedbackType,
+      comment,
+      screenshot
     })
 
+    setIsSendingFeedback(false)
     onFeedbackSent()
   }
 
@@ -56,25 +64,25 @@ export function FeedbackContentStep({
       </header>
 
       <form className="my-4 w-full">
-        <textarea 
+        <textarea
           className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-offset-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder={feedbackTypeInfo.placeholder}
           onChange={event => setComment(event.target.value)}
         />
 
         <footer className="flex gap-2 mt-2">
-          <ScreenshotButton 
+          <ScreenshotButton
             screenshot={screenshot}
             onScreenshotTook={setSreenshot}
           />
 
           <button
             type="submit"
-            disabled={comment.length === 0}
+            disabled={comment.length === 0 || isSendingFeedback}
             onClick={handleSubmitFeedback}
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus-ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
           >
-            Enviar feedback
+            { isSendingFeedback ? <Loading /> : 'Enviar feedback' }
           </button>
         </footer>
       </form>
